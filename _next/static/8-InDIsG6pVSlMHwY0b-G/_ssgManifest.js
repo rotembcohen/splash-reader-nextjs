be33bd87cdbc50f7dc97577d80e6e5ff18adf58a
rotembcohen <rotembcohen@gmail.com>
2025-03-23 10:53:04 -0400

    Updates

---
+++ _next/static/8-InDIsG6pVSlMHwY0b-G/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[lang]","\u002F[lang]\u002Fcontact","\u002F[lang]\u002Fenvironments","\u002F[lang]\u002Ffeatures","\u002F[lang]\u002Freviews"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
